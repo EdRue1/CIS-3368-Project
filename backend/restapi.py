94d839b891fde418b09256fd58079fa9bd5cb21e
EdRue1 <efrueda-pliego@uh.edu>
2024-03-04 21:22:29 -0600

    Final commit v1. Cleaned up comments and full security functionality

--- backend/restapi.py
+++ backend/restapi.py
@@ -2,18 +2,14 @@
 import flask
 import hashlib
 import math
-
 from flask import jsonify
 from flask import request, make_response
 from functools import wraps
-
 import sql
 from sql import create_connection
 from sql import execute_read_query
 from sql import execute_update_query
-
 import creds
-
 
 #create application with configuration
 app = flask.Flask(__name__)
@@ -23,18 +19,7 @@ masterPassword = "c9f60108ce2cf9d828075088235383616d7586cbe9ee31a7032218ed434240
 masterUsername = 'username'
 
 # basic http authentication, prompts username and password upon contacting the endpoint
-
-# 'password' as plaintext should not be used to verify authorization to access. 
-# the password should be hashed and the hash should be compared to the stored password hash in the database
-"""@app.route('/authenticatedroute', methods=['GET'])
-def auth_test():
-    if request.authorization:
-        encoded = request.authorization.password.encode() #unicode encoding
-        hasedResult = hashlib.sha256(encoded) #hashing
-        if request.authorization.username == masterUsername and hasedResult.hexdigest() == masterPassword:
-            return '<h1> Authorized user access </h1>'
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
-"""
+#information on wraps was learned from https://circleci.com/blog/authentication-decorators-flask/
 def basic_authentication(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -57,8 +42,8 @@ def home():
 #//////////////////////////////FACILITY///////////////////////////////////////////////////////
 #get one facility
 @app.route('/api/facility', methods=['GET'])
+@basic_authentication
 def api_facility_by_id():
-   
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
@@ -75,6 +60,7 @@ def api_facility_by_id():
 
 #get all facilities
 @app.route('/api/facility/all', methods=['GET'])
+@basic_authentication
 def all_facility_info():
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
@@ -84,18 +70,19 @@ def all_facility_info():
 
 #add a facility
 @app.route('/api/facility', methods=['POST'])
+@basic_authentication
 def api_add_facility():
     request_data = request.get_json()
     newFacname = request_data['name']
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "insert into facility(name) values ('%s')" % (newFacname)
-
     execute_update_query(myconn, sql)
     return 'Add facility request successful!'
 
 #update at id#
 @app.route('/api/facility', methods=['PUT'])
+@basic_authentication
 def api_update_facility_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
@@ -103,30 +90,26 @@ def api_update_facility_byID():
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "UPDATE facility SET name = '%s' WHERE id = '%s'" % (newFacname, idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Update request successful!"
 
 #delete at id#
 @app.route('/api/facility', methods=['DELETE'])
+@basic_authentication
 def api_delete_facility_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
-    
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "DELETE FROM facility where id = '%s'" % (idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Delete request successful!"
 
 #/////////////////////////////////////CLASSROOM////////////////////////////////////////////////
 #get one classroom
 @app.route('/api/classroom', methods=['GET'])
+@basic_authentication
 def api_classroom_by_id():
-   
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
@@ -143,6 +126,7 @@ def api_classroom_by_id():
 
 #get all classrooms
 @app.route('/api/classroom/all', methods=['GET'])
+@basic_authentication
 def all_classroom_info():
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
@@ -152,55 +136,50 @@ def all_classroom_info():
 
 #add a classroom
 @app.route('/api/classroom', methods=['POST'])
+@basic_authentication
 def api_add_classroom():
     request_data = request.get_json()
     newcapa = request_data['capacity']
     newname = request_data['name']
     newfaca = request_data['facility']
-
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "insert into classroom(capacity, name, facility) values ('%s','%s','%s')" % (newcapa, newname, newfaca)
-
     execute_update_query(myconn, sql)
     return 'Add classroom request successful!'
 
 #update at id#
 @app.route('/api/classroom', methods=['PUT'])
+@basic_authentication
 def api_update_classroom_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
     newcapa = request_data['capacity']
     newname = request_data['name']
     newfaca = request_data['facility']
-    
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "UPDATE classroom SET capacity = '%s', name = '%s', facility = '%s' WHERE id = '%s'" % (newcapa, newname, newfaca, idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Update request successful!"
 
 #delete at id#
 @app.route('/api/classroom', methods=['DELETE'])
+@basic_authentication
 def api_delete_plant_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
-    
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "DELETE FROM classroom where id = '%s'" % (idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Delete request successful!"
 
 #////////////////////////////////////////TEACHER//////////////////////////////////////////////////
 #get one teacher
 @app.route('/api/teacher', methods=['GET'])
+@basic_authentication
 def api_teacher_by_id():
-   
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
@@ -217,6 +196,7 @@ def api_teacher_by_id():
 
 #get all teachers
 @app.route('/api/teacher/all', methods=['GET'])
+@basic_authentication
 def all_teachers_info():
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
@@ -226,55 +206,50 @@ def all_teachers_info():
 
 #add a teacher
 @app.route('/api/teacher', methods=['POST'])
+@basic_authentication
 def api_add_teacher():
     request_data = request.get_json()
     newfname = request_data['firstname']
     newlname = request_data['lastname']
     newroom = request_data['room']
-
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "insert into teacher(firstname, lastname, room) values ('%s','%s','%s')" % (newfname, newlname, newroom)
-
     execute_update_query(myconn, sql)
     return 'Add teacher request successful!'
 
 #update at id#
 @app.route('/api/teacher', methods=['PUT'])
+@basic_authentication
 def api_update_teacher_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
     newfname = request_data['firstname']
     newlname = request_data['lastname']
     newroom = request_data['room']
-    
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "UPDATE teacher SET firstname = '%s', lastname = '%s', room = '%s' WHERE id = '%s'" % (newfname, newlname, newroom, idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Update teacher request successful!"
 
 #delete at id#
 @app.route('/api/teacher', methods=['DELETE'])
+@basic_authentication
 def api_delete_teacher_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
-    
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
     sql = "DELETE FROM teacher where id = '%s'" % (idtoupdate)
-
     execute_update_query(myconn, sql)
-        
     return "Delete request successful!"
 
 #//////////////////////////////////////////CHILD////////////////////////////////////////////////////
 #get one child
 @app.route('/api/child', methods=['GET'])
+@basic_authentication
 def api_child_by_id():
-   
     if 'id' in request.args:
         id = int(request.args['id'])
     else:
@@ -291,6 +266,7 @@ def api_child_by_id():
 
 #get all children
 @app.route('/api/child/all', methods=['GET'])
+@basic_authentication
 def all_child_info():
     mycreds = creds.creds()
     myconn = create_connection(mycreds.myhostname, mycreds.uname, mycreds.passwd, mycreds.dbname)
@@ -300,6 +276,7 @@ def all_child_info():
 
 #add a child
 @app.route('/api/child', methods=['POST'])
+@basic_authentication
 def api_add_child():
     request_data = request.get_json()
     newfname = request_data['firstname']
@@ -316,7 +293,6 @@ def api_add_child():
     for child in childlist:
         if child['room']== newroom:
             childclasscount = childclasscount + 1
-    #return str(childclasscount)
 
     #count all teachers to a room
     mysqlst = "select * from teacher"
@@ -325,7 +301,6 @@ def api_add_child():
     for teacher in teacherlist:
         if teacher['room']== newroom:
             teacherclasscount = teacherclasscount + 1
-    #return str(teacherclasscount)
 
     #check capacity of a room
     mysqlst = "select * from classroom"
@@ -335,7 +310,6 @@ def api_add_child():
         if classroom['id']== newroom:
             results.append(classroom)
     capacitycount = results[0]['capacity']
-    #return str(capacitycount)
 
     #if all conditions met, then accept child into classroom
     if (childclasscount < capacitycount) and (math.ceil(childclasscount/10) <= teacherclasscount):
@@ -348,6 +322,7 @@ def api_add_child():
    
 #update at id#
 @app.route('/api/child', methods=['PUT'])
+@basic_authentication
 def api_update_child_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
@@ -355,7 +330,6 @@ def api_update_child_byID():
     newlname = request_data['lastname']
     newage = request_data['age']
     newroom = request_data['room']
-
 
     #count all childs in a room
     mycreds = creds.creds()
@@ -367,9 +341,9 @@ def api_update_child_byID():
     for child in childlist:
         if child['room']== newroom:
             childclasscount = childclasscount + 1
+        #check if the child being updated wil remain in the same class
         if (child['id'] == idtoupdate) and (child['room'] == newroom):
             sameRoomFlag = sameRoomFlag + 1
-
 
     #count all teachers to a room
     mysqlst = "select * from teacher"
@@ -389,6 +363,7 @@ def api_update_child_byID():
     capacitycount = results[0]['capacity']
 
     #if all conditions met, then accept child update
+    #sameRoomFlag will check if the child will remain in the same room
     if sameRoomFlag == 1:
         sql = "UPDATE child SET firstname = '%s', lastname = '%s', age = '%s', room = '%s' WHERE id = '%s'" % (newfname, newlname, newage, newroom, idtoupdate)
         execute_update_query(myconn, sql)
@@ -400,9 +375,9 @@ def api_update_child_byID():
     else:
         return 'Class full or something went wrong'
 
-
 #delete at id#
 @app.route('/api/child', methods=['DELETE'])
+@basic_authentication
 def api_delete_child_byID():
     request_data = request.get_json()
     idtoupdate = request_data['id']
@@ -415,7 +390,5 @@ def api_delete_child_byID():
         
     return "Delete request successful!"
 
-
 #run the application to listen for user requests
 app.run()
-
